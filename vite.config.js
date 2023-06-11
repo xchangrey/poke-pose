e9832f98d71c1c103d5ba284ff11e69ab12396c4
xchangrey <geroychristian@gmail.com>
2023-06-11 19:13:12 +0800

    fix: failing build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Specify the entry point for your application
+      input: {
+        main: 'src/main.jsx',
+      }, // Adjust the path if needed
+
+      // Add any explicit optimizeDeps.include patterns
+      // to ensure the necessary dependencies are pre-bundled
+      optimizeDeps: {
+        include: ['dependency-package'], // Adjust with your dependencies
+      },
+    },
+  },
+  plugins: [
+    react(),
+    reactRefresh(),
+  ],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    framework: 'jest',
+    include: 'src/components/**/*.test.(ts|tsx)'
+  },
 })
