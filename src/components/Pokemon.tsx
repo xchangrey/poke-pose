485c81913e7f4abbdbac8e321c2c00ef548b7d49
xchangrey <geroychristian@gmail.com>
2023-06-11 19:48:23 +0800

    test: add Pokemon test

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -36,8 +36,18 @@ const Pokemon = (): ReactElement => {
             className="flex items-center bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded m-8"
             onClick={handleBackClick}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              fill="none" viewBox="0 0 24 24" 
+              strokeWidth="1.5" 
+              stroke="currentColor" 
+              className="w-6 h-6 mr-2"
+            >
+              <path 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" 
+              />
             </svg>
             Back
           </button>
